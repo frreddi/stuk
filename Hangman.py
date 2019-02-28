dc681cda521836341a9bff9f1205ca53a0d0de76
frreddi <43642962+frreddi@users.noreply.github.com>
2019-02-28 22:49:25 +0100

    Stuk is alpha omega
    pepe

--- Hangman.py
+++ Hangman.py
@@ -6,6 +6,7 @@ try:
         right = [];
         guess = str('!1984!');
         secret_word = str('!1984!');
+        numberOfGuesses = int(-1984);
         tog = bool(False);
         
         while not secret_word.isalpha():
@@ -14,17 +15,22 @@ try:
                 print('Only letters here XY/XX!');
         hiddenList = list('*' * len(secret_word));
         
-        numberOfGuesses = int(input('Now how many guesses do you wanna give them, integers only: '));
+        while numberOfGuesses <= 0:
+            numberOfGuesses = abs(int(input('Now, how many guesses do you wanna give them? Natural numbers only only: ')));
+            print();
+            if numberOfGuesses <= 0:
+                print('I guess you most have guesses to guess, or...?');
         
         print('\n'*42);
         print('\n'*13);
         
         
-        print('The secret word has', len(secret_word), 'letters, and you have', numberOfGuesses, 'guesses.\n');
+        print('Welcome. The secret word has', len(secret_word), 'letters, and you have', numberOfGuesses, 'guesses.\n');
         
         while oneMore == '':
             
             if tog == True:
+                print();
                 if numberOfGuesses == 1:
                     print('Last try!');
                 else:
@@ -32,32 +38,34 @@ try:
                 print('Guessed:', ''.join(sorted(all)));
                 print('Right guesses:', ''.join(sorted(right)));
             tog = True;
+            print();
             print(''.join(hiddenList));
             
             if ''.join(hiddenList) == secret_word:
                 print('\nCongratulations, you found the secret word! It was:', secret_word + ', GG :D');   #Win animation
                 break;
             if numberOfGuesses == 0:
-                print("\nYou are out of guesses. Op fail'd");   #Loss animation
+                print("\nYou are out of guesses. Op fail'd :/");   #Loss animation
                 break;
             
             del guess;
             guess = str.upper(input('\nGuess a letter in the secret word: '));
-            print('\n');
-            if not guess.isalpha():
-                print('Only a letter here XY/XX!');
-                continue;
+            print('\n'*3);
+            print('Guessed:', guess);
             if len(guess)!=1:
                 print('Plz guess at one letter at a time...');
                 continue;
+            if not guess.isalpha():
+                print('Only a letter here XY/XX!');
+                continue;
             if guess in all:
-                print('You already guessed this.');
+                print('You already guessed this :3');
                 continue;
             all.append(guess);
             
             if guess in secret_word:
                 right.append(guess);
-                print('You guessed a correct letter! GJ');
+                print('You guessed a correct letter, GJ!');
                 for x in range (len(secret_word)):
                     if secret_word[x] == guess:
                         del hiddenList[x];
@@ -67,12 +75,12 @@ try:
                 numberOfGuesses-=1;
                 print('Sorry, wrong guess. Better luck next time.');   #Draw here
         
-        oneMore = input('Press enter for a new game.');
+        oneMore = input('Press enter for a new game: ');
         if oneMore != '':
             break;
 
 except:
-    input('An error occurred. Press a key to exit.');
+    input('\nAn error occurred :o Press a key to exit. ');
     quit();
 
 finally:
