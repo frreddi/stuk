4c57d11ca9b88d4011793a0516c56fca199d5673
frreddi <43642962+frreddi@users.noreply.github.com>
2020-12-14 08:48:27 +0100

    Primordial-Sunk-Totenglocke

---
+++ python/cosmicHydragon.py
@@ -0,0 +1,20 @@
+
+g = 4   #input generator base
+n = 1080    #input modular group
+u = 42    #input pick exponent
+
+styList = []
+go = True
+spartanEyro = 0
+while go:
+    
+    val = (g**spartanEyro)%n
+    for el in styList:
+        if el == val:
+            go = False
+    styList.append(val)
+    
+    print("(",g,"^",spartanEyro,") mod",n," = ",val)
+    spartanEyro += 1
+
+print("\n(",g,"^",u,") mod",n," = ",(g**u)%n)
