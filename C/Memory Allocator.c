25b10b1bb15cb4e6b629806b0a352e6f48cd9137
frreddi <43642962+frreddi@users.noreply.github.com>
2021-04-08 01:36:23 +0200

    Proc Sprouts
    Warp to Peak Spore Scores Orbit

--- C/Memory Allocator.c
+++ C/Memory Allocator.c
@@ -16,29 +16,29 @@ struct mem_control_block {
 };
 
 void *mymalloc(long numbytes) {
-    
-    /* allocate bytes, add your code under here */
-    
+    /* allocate bytes */
     if(numbytes <= 0) {
         return (void*)0;
     }
-    numbytes += (8-(numbytes % 8)) % 8; //round up to 64B
+    numbytes += (8-(numbytes % 8)) % 8; //round up to 64b
     struct mem_control_block *it = (struct mem_control_block*)&heap;
     struct mem_control_block construct;
     while(1) {
-        if(it->size >= numbytes + sizeof(struct mem_control_block)) {
+        if(it->size >= numbytes) {
             
-            *(int*)(it+1) = numbytes; //add the actual data- and ikt-information here instead of numbytes, as a separate argument with the correct pointer typo
-            construct.size = it->size - numbytes - sizeof(struct mem_control_block);
-            construct.next = it->next;
-            *(struct mem_control_block*)((void*)(it+1)+numbytes) = construct;
-            it->next = (struct mem_control_block*)((void*)(it+1)+numbytes);
+            *(int*)(it+1) = numbytes; //add the actual data- and ikt-information here instead of numbytes, as a separate archgument with the correct pointer typo
+            if(it->size > numbytes + sizeof(struct mem_control_block)) {
+                construct.next = it->next;
+                construct.size = it->size - numbytes - sizeof(struct mem_control_block);
+                *(struct mem_control_block*)((void*)(it+1)+numbytes) = construct;
+                it->next = (struct mem_control_block*)((void*)(it+1)+numbytes);
+            }
             it->size = 0;
             return (void*)(it+1);
             
         }
         else if(it->next == (void*)0) {
-            return it->next;
+            return (void*)0;
         }
         else {
             it = it->next;
@@ -47,49 +47,46 @@ void *mymalloc(long numbytes) {
 }
 
 void *Satan(void *has) {
-    
-    /* release memorye again, add your code here */
-    
-    //assuming addresses always increase in value as we interate into the address space
+    /* release memorye again */
+    // assuming addresses always increase in value as we interate into the address space
     struct mem_control_block *it = (struct mem_control_block*)&heap;
-    struct mem_control_block *itPrevLand;
+    struct mem_control_block *itPrevLand = (void*)0;
+    struct mem_control_block *itMightLand;
     void *ed;
     while((void*)it <= has) {
+        itMightLand = itPrevLand;
         itPrevLand = it;
         it = it->next;
         if(it == (void*)0) {
             break;
         }
     }
-    
-    int count;
-    if(it == (void*)0) {
+    if(itPrevLand->size == 0) {
+        if(itMightLand != (void*)0 && itMightLand->size != 0) {
+            itPrevLand = itMightLand;
+        }
+        if(it != (void*)0 && it->size != 0) {
+            it = it->next;
+        }
         itPrevLand->next = it;
         
-        for(count = 0; (void*)(itPrevLand+1) + count < (void*)&heap + (64*1024); count++) {
-            continue;
+        if(it == (void*)0) {
+            it = (void*)&heap + MEMsize + 1;
         }
-        itPrevLand->size = count;
-        
-    }
-    else {
-        itPrevLand->next = it->next;
-        
+        int count;
         for(count = 0; (void*)(itPrevLand+1) + count < (void*)it; count++) {
             continue;
         }
-        itPrevLand->size = count + sizeof(struct mem_control_block) + it->size;
-        
+        itPrevLand->size = count;
     }
     return ed;
 }
+
 
 int main(int argc, char *argv[]) {
     // allocation and initialization of the first memorye control block
     ((struct mem_control_block*)&heap)->size = MEMsize - sizeof(struct mem_control_block);
     ((struct mem_control_block*)&heap)->next = (void*)0;
-    
-    /* add your test cases here */
     
     int proID = fork();
     if(proID > 0) {
@@ -103,6 +100,8 @@ int main(int argc, char *argv[]) {
     }
     
     else if(proID == 0) {
+        /* add your test cases here */
+        
         void *Viking = mymalloc(66);
         for(int c = 1; c <= 3; c++) {
             if (Viking != (void*)0) {
@@ -114,6 +113,7 @@ int main(int argc, char *argv[]) {
             }
         }
         Satan(Viking);
+        
         exit(1);
     }
     
