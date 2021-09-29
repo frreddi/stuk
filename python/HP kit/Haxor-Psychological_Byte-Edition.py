4df5ba0511a07fb8a90f659a982d662b561d1092
frreddi <43642962+frreddi@users.noreply.github.com>
2021-09-29 04:00:12 +0200

    life

--- python/HP kit/Haxor-Psychological_Byte-Edition.py
+++ python/HP kit/Haxor-Psychological_Byte-Edition.py
@@ -2,12 +2,12 @@ from random import randint;
 from time import sleep;
 
 
-tog = bool(0);
-while tog==0:
+tog = bool(1);
+while tog==1:
     try:
         numb = int(0b110101111110101111110101111110101111110101111110101111);
         #numb = int(input("Input digits: "));
-        tog ^= 1;
+        tog = 0;
     except:
         print("Only whole numbers");
         continue;
@@ -35,7 +35,7 @@ while True:
         rand = randint(1, 50);
         if rand>36:
             randBit = randint(0, 7);
-            numb ^= (1<<(randBit+(n*8)));
+            numb ^= 1<<(randBit+(n*8));
     
     for n in range (0, rand):
         print(bin(numb));
